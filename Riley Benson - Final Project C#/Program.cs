5282792437447771b63c37bb03bd359392730477
RIBOIMAN1 <rileybenson@my.whccd.edu>
2025-02-27 03:00:16 -0800

    Added documentation comments to the top of the code and aadded a try-catch inside of the program class file, and added new files named "Active.cs" for managing active subscriptions, "Edit.cs" for managing editing subscriptions, "Delete.cs" for managing deleting subscriptions, and "SubManagement.cs" for a more general management for the service / manager.

--- Riley Benson - Final Project C#/Program.cs
+++ Riley Benson - Final Project C#/Program.cs
@@ -1,41 +1,60 @@
-﻿namespace Riley_Benson___Final_Project_C_
+﻿// Author: Riley Benson
+// Course: COMP-003A
+// Faculty: Jonathan Cruz
+// Purpose: To showcase what I have learned during my 8 weeks in this C# course by making a video game subscription service / manager.
+
+namespace Riley_Benson___Final_Project_C_
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-			bool programActive = true;
-			Console.WriteLine("Welcome to Pixel Hub!");
+            bool programActive = true;
+            Console.WriteLine("Welcome to Pixel Hub!");
             Console.WriteLine("Your Premium Video Game Suvbscription Manager!");
             Console.WriteLine("\nChoose from one of the following options to get started on your journey to better spending habits:");
-            Console.WriteLine("\n1. Add a subscription");
-            Console.WriteLine("2. View active subscriptions");
-			Console.WriteLine("3. Edit subscription");
-			Console.WriteLine("4. Delete subscription");
-			Console.WriteLine("5. Exit");
-            Console.Write("Enter your choice below (1-5): ");
-			int choice = int.Parse(Console.ReadLine());
-			if (choice == 1)
+            while (programActive)
             {
-                
-            }
-            else if (choice == 2)
-            {
-                
-            }
-            else if (choice == 3)
-            {
-                
-            }
-            else if (choice == 4)
-            {
-                
-            }
-            else if (choice == 5)
-			{
-				programActive = false;
-				Console.WriteLine("Thank you for using Pixel Hub!");
+                try
+                {
+					Console.WriteLine("\n1. Add a subscription");
+					Console.WriteLine("2. View active subscriptions");
+					Console.WriteLine("3. Edit subscription");
+					Console.WriteLine("4. Delete subscription");
+					Console.WriteLine("5. Exit");
+					Console.Write("Enter your choice below (1-5): ");
+					int choice = int.Parse(Console.ReadLine());
+					if (choice == 1)
+					{
+						
+					}
+					else if (choice == 2)
+					{
+						
+					}
+					else if (choice == 3)
+					{
+						
+					}
+					else if (choice == 4)
+					{
+						
+					}
+					else if (choice == 5)
+					{
+						programActive = false;
+						Console.WriteLine("Thank you for using Pixel Hub!");
+					}
+					else
+					{
+						Console.WriteLine("Your choice is invalid, please choose a number between 1 and 5.");
+					}
+				}
+				catch (Exception ex)
+				{
+					Console.WriteLine($"Error: {ex.Message}");
+				}
 			}
 		}
-    }
+	}
 }
