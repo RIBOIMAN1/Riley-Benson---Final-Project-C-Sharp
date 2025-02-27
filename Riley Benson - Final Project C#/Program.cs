f820ba57693e1d7ce760dc421c03fd1d129302cd
RIBOIMAN1 <rileybenson@my.whccd.edu>
2025-02-27 00:28:06 -0800

    Added skeleton base code to my program class, making it give the user greeting messages, tells the user how to operate the program, gives them a list of options to manage their subscription, and contains blank if, else if, and else statements for each choice. There is an exception for choice #5, where it uses a bolean to end the program.

--- Riley Benson - Final Project C#/Program.cs
+++ Riley Benson - Final Project C#/Program.cs
@@ -4,7 +4,38 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
+			bool programActive = true;
+			Console.WriteLine("Welcome to Pixel Hub!");
+            Console.WriteLine("Your Premium Video Game Suvbscription Manager!");
+            Console.WriteLine("\nChoose from one of the following options to get started on your journey to better spending habits:");
+            Console.WriteLine("\n1. Add a subscription");
+            Console.WriteLine("2. View active subscriptions");
+			Console.WriteLine("3. Edit subscription");
+			Console.WriteLine("4. Delete subscription");
+			Console.WriteLine("5. Exit");
+            Console.Write("Enter your choice below (1-5): ");
+			int choice = int.Parse(Console.ReadLine());
+			if (choice == 1)
+            {
+                
+            }
+            else if (choice == 2)
+            {
+                
+            }
+            else if (choice == 3)
+            {
+                
+            }
+            else if (choice == 4)
+            {
+                
+            }
+            else if (choice == 5)
+			{
+				programActive = false;
+				Console.WriteLine("Thank you for using Pixel Hub!");
+			}
+		}
     }
 }
