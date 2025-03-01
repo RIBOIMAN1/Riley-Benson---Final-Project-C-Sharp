7375972a3ee61a022eb4508853ab8a9d3ffb87b3
RIBOIMAN1 <rileybenson@my.whccd.edu>
2025-02-28 17:29:58 -0800

    Added code that makes it so that the user can add subscriptions to the list of subscriptions, adding a public method with that code that prompts the user to enter the name of the subscription, the per month price of the subscription, and the amount of months their subscription will last. There are also thrown exception errors that account for empty and null inputs for the name prompt and values that are less than or equal to 0 for the price of the subscription and the amount of months the subscription will last, and this all accounts for the "Add" class.

--- Riley Benson - Final Project C#/Add.cs
+++ Riley Benson - Final Project C#/Add.cs
@@ -6,8 +6,34 @@ using System.Threading.Tasks;
 
 namespace Riley_Benson___Final_Project_C_
 {
+	/// <summary>
+	/// Gives the user the ability to add subscriptions to the service.
+	/// </summary>
 	public class Add
     {
-        
-    }
+		/// <summary>
+		/// Adds an inputted subscription to the list of already existing subscriptions.
+		/// </summary>
+		/// <param name="subs">List of subscriptions</param>
+		public void AddSubscription(List<SubManagement> subs)
+        {
+			SubManagement newSub = new SubManagement();
+            Console.WriteLine("\nAdding new subscription...");
+			// Obtains the name of the subscription.
+			Console.Write("Enter the name of your subscription: ");
+			string name = Console.ReadLine();
+			if (string.IsNullOrWhiteSpace(name))
+				throw new ArgumentException("Empty and null inputs are invalid, try again.");
+			// Obtains the price of the subscription.
+			Console.Write("Enter the per-month price of your subscription: $");
+			double price;
+			if (!double.TryParse(Console.ReadLine(), out price) || price <= 0)
+				throw new ArgumentException("The price cannot be less than or equal to 0, try again.");
+			// Obtains the amount of months that the subscription will last.
+			Console.Write("Enter the amount of months your subscription will last: ");
+			int months;
+			if (!int.TryParse(Console.ReadLine(), out months) || months <= 0)
+				throw new ArgumentException("The amount of months your subscription will last cannot be less than or equal to 0, try again.");
+		}
+	}
 }
