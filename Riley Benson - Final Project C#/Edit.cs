8b4095cf691c7f967c0959e5ffecb4d5c70f9e17
RIBOIMAN1 <rileybenson@my.whccd.edu>
2025-03-01 16:57:33 -0800

    Add a code that lets the user edit a subscription of their choice from their list of subscriptions, letting them edit the name, price, and duration in months of the subscription they choose all at the same time, added code that checks for an empty subscription list, added code that lists the users subscriptions to them, prompts the user to select one of their subscriptions to edit, and added code that handles user input within the program.

--- Riley Benson - Final Project C#/Edit.cs
+++ Riley Benson - Final Project C#/Edit.cs
@@ -6,8 +6,39 @@ using System.Threading.Tasks;
 
 namespace Riley_Benson___Final_Project_C_
 {
+    /// <summary>
+    /// Lets the user edit a subscription of their choice from their list of subscriptions.
+    /// </summary>
     public class Edit
     {
-        
+        /// <summary>
+        /// Gives the user the choice to edit the name, price, and duration in months of the subscription they choose.
+        /// </summary>
+        /// <param name="subs">List of already existing subscriptions</param>
+        public void EditSubscription(List<SubManagement> subs)
+        {
+            // Checks for an empty subscription list before proceeding with the rest of the program.
+            if (subs.Count == 0)
+            {
+                Console.WriteLine("There are no subscriptions currently in your list, therefore none can be edited.");
+				Console.WriteLine("Returning to the main menu...");
+                return;
+			}
+            Console.WriteLine("\nGetting ready to edit subscription...");
+            /// List the different subscriptions the user of the program has to them.
+            Console.WriteLine("\nBelow is your current list of subscriptions: ");
+			for (int i = 0; i < subs.Count; i++)
+			{
+				Console.WriteLine($"{i + 1}. {subs[i].FetchSubName()}");
+			}
+            // Prompts the user to select one of their subscriptions to edit.
+            Console.WriteLine("\nChoose from the list of subscriptions which one you want to edit (1, 2, 3, etc.): ");
+            int editChoice;
+			// Handles the input of the user in the program.
+			if (!int.TryParse(Console.ReadLine(), out editChoice) || editChoice < 1 || editChoice > subs.Count)
+			{
+				throw new ArgumentException($"Your choice is invalid, please choose a number between 1 and {subs.Count}.");
+			}
+		}
     }
 }
